d6d42b548d3309bccb5add53426e776f1847ac07
ColtG-py <80473907+ColtG-py@users.noreply.github.com>
2023-08-11 10:58:26 -0400

    adding twitter

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,11 +6,16 @@ export default function Navbar() {
     return (
         <nav className="bg-slate-680 p-4 top-0 drop-shadow-xl z-10">
             <div className="prose prose-xl mx-auto flex justify-between flex-col sm:flex-row">
-                <h1 className={`${robotoMono.className} ml-auto mr-auto underlinetext-3xl font-extrabold grid place-content-center mb-2 md:mb-0 text-2xl font-bold grid place-content-center mb-2 md:mb-0`} >
+                <h1 className={`${robotoMono.className} underlinetext-3xl font-extrabold grid place-content-center mb-2 md:mb-0 text-2xl font-bold grid place-content-center mb-2 md:mb-0`} >
                     <Link href="/" className="text-white-700 text-white/90 no-underline hover:text-white">
                         Every Weekend. Every Game.
                     </Link>
                 </h1>
+                <div className="flex flex-row justify-center sm:justify-evenly align-middle gap-4 text-white text-4xl lg:text-5xl">
+                    <Link className="text-white/90 hover:text-white" href="https://twitter.com/InsideChip">
+                        <FaTwitter />
+                    </Link>
+                </div>
             </div>
         </nav>
     )
